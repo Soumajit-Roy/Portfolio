efe2293a1bcf2bc6555ac956972b45ee53fe22e6
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-08-24 22:21:40 +0530

    reworking on grid

--- src/Components/Works/works.jsx
+++ src/Components/Works/works.jsx
@@ -5,9 +5,16 @@ export default function Works() {
   return (
     <div className="works-el">
       <div className="project1">
-        <img src="https://d33wubrfki0l68.cloudfront.net/66be029b8c817a0008cdd2d3/screenshot_2024-08-15-13-29-24-0000.webp"></img>
+        <div className="prj-img">
+        <img  src="https://d33wubrfki0l68.cloudfront.net/66be029b8c817a0008cdd2d3/screenshot_2024-08-15-13-29-24-0000.webp"></img>
+        </div>
+        <div className="txt-header">
         <h2>Simple Travel Journal</h2>
+        </div>
+        <div className="txt-desc">
         <p>Node React HTML CSS JavaScript</p>
+        </div>
+        <div className="prj-icons">
         <a
           href="https://github.com/Soumajit-Roy/Simple-Travel-Journal"
           target="_blank"
@@ -17,11 +24,15 @@ export default function Works() {
         <a href="https://travel-log-responsive.netlify.app/" target="_blank">
           <i class="fa-solid fa-arrow-up-right-from-square fa-lg"></i>
         </a>
+        </div>
       </div>
+
       <div className="project2">
         <img src="https://d33wubrfki0l68.cloudfront.net/66c61a274bcb5e27095042be/screenshot_2024-08-21-16-47-54-0000.webp"></img>
+        <div className="work-text">
         <h2>Basic Ecom Website</h2>
         <p>Bootstrap HTML CSS JavaScript</p>
+        <div className="prj-icons">
         <a href="https://github.com/Soumajit-Roy/Ecom-Website" target="_blank">
           <i class="fa-brands fa-github fa-lg"></i>
         </a>
@@ -31,11 +42,15 @@ export default function Works() {
         >
           <i class="fa-solid fa-arrow-up-right-from-square fa-lg"></i>
         </a>
+        </div>
+        </div>
       </div>
       <div className="project3">
         <img src="https://d33wubrfki0l68.cloudfront.net/66b5bb1311461b125f8f6454/screenshot_2024-08-09-06-46-06-0000.webp"></img>
+        <div className="work-text">
         <h2>Business Banner Card</h2>
         <p>Node React HTML CSS JavaScript</p>
+        <div className="prj-icons">
         <a
           href="https://github.com/Soumajit-Roy/business-banner-card"
           target="_blank"
@@ -45,12 +60,16 @@ export default function Works() {
         <a href="https://business-banner-card.netlify.app/" target="_blank">
           <i class="fa-solid fa-arrow-up-right-from-square fa-lg"></i>
         </a>
+        </div>
+        </div>
       </div>
 
       <div className="project4">
         <img src="https://d33wubrfki0l68.cloudfront.net/66a1144c7bfc2c000870b428/screenshot_2024-07-24-14-49-08-0000.webp"></img>
+        <div className="work-text">
         <h2>Simple Password Generator</h2>
         <p>HTML CSS Javascript</p>
+        <div className="prj-icons">
         <a
           href="https://github.com/Soumajit-Roy/Simple-Password-Generator-JS"
           target="_blank"
@@ -60,6 +79,8 @@ export default function Works() {
         <a href="https://simple-password-gens.netlify.app/" target="_blank">
           <i class="fa-solid fa-arrow-up-right-from-square fa-lg"></i>
         </a>
+        </div>
+        </div>
       </div>
     </div>
   );
