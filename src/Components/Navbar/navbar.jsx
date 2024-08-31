c6a273535f0ebc469a9a753ae9b8111730ec9f81
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-08-31 22:14:00 +0530

    done

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./navbar.css";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  
   return (
     <div className="nav-wrapper">
     <div className="nav-el">
       <div className="name">
         <p>Soumajit</p>
       </div>
-      <div className="links">
+      <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+      <div className={`links ${isMenuOpen ? 'active' : ''}`}>
         <a href="#abt-tag">About</a>
         <a href="#ctc-tag">Contact</a>
       </div>
