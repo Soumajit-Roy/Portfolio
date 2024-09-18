76197308c62e062727f39872510e79575805e9a0
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-09-19 04:00:07 +0530

    replace avatars & added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import "./styles/hover-min.css"
+import "./styles/hover-min.css";
 import Navbar from "./Components/Navbar/navbar";
 import Hero from "./Components/Hero/hero";
 import Works from "./Components/Works/works";
 import About from "./Components/AboutMe/about";
 import Contact from "./Components/Contact/contact";
-import ContactForm from "./Components/ContactForm/ContactForm";
 import Footer from "./Components/Footer/footer";
+import Preloader from "./Components/preloader/preloader";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); //time
+  }, []);
+
   return (
     <div className="container">
-      <Navbar />
-      <Hero />
-      <Works />
-      <About />
-      <Contact />
-      {/* <ContactForm /> */}
-      <Footer />
-      </div>
+      {loading ? (
+        <Preloader /> 
+      ) : (
+        <>
+          {/* Main Content is displayed once loading is false */}
+          <Navbar />
+          <Hero />
+          <Works />
+          <About />
+          <Contact />
+          <Footer />
+        </>
+      )}
+    </div>
   );
 }
---
+++ src/Components/preloader/preloader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './preloader.css'
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <span class="loader"></span>
+    </div>
+  );
+};
+
+export default Preloader;
